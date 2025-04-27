5b8c3245c66516cd2af0d4eb82398782d5e33e20
ahmaddeveloper17 <125741237+ahmaddeveloper17@users.noreply.github.com>
2025-04-27 14:12:18 +0500

    add ai chatbot

--- app/index.tsx
+++ app/index.tsx
@@ -4,12 +4,11 @@ import { Card } from "@/components/ui/card";
 import { Grid, GridItem } from "@/components/ui/grid";
 import { Image } from "@/components/ui/image";
 import { Link } from "expo-router";
-import { Linking, ScrollView, StyleSheet, Text, } from "react-native";
+import { Linking, ScrollView, StyleSheet, Text } from "react-native";
 
 export default function Index() {
-
   const handleCall = async () => {
-    const phoneNumber = '1122';
+    const phoneNumber = "1122";
     const url = `tel:${phoneNumber}`;
     try {
       const supported = await Linking.canOpenURL(url);
@@ -34,11 +33,7 @@ export default function Index() {
         <GridItem
           _extra={{ className: "col-span-10 md:col-span-6 lg:col-span-3" }}
         >
-          <Card
-            size="lg"
-            variant="filled"
-            style={styles.card}
-          >
+          <Card size="lg" variant="filled" style={styles.card}>
             <Image
               source={require("../assets/images/quick-guid.png")}
               alt="Quick Guid"
@@ -48,17 +43,21 @@ export default function Index() {
             <Text style={{ textAlign: "center", marginTop: 10 }}>
               Get instant help with first aid and CPR questions.
             </Text>
-            <Button size="lg" variant="solid" style={{ backgroundColor: "#fe2238", marginTop: 20 }}>
-              <ButtonText>Start Guidance</ButtonText>
-            </Button>
+            <Link href={"/chat"} asChild>
+              <Button
+                size="lg"
+                variant="solid"
+                style={{ backgroundColor: "#fe2238", marginTop: 20 }}
+              >
+                <ButtonText>Start Guidance</ButtonText>
+              </Button>
+            </Link>
           </Card>
         </GridItem>
         <GridItem
           _extra={{ className: "col-span-10 md:col-span-6 lg:col-span-3" }}
         >
-          <Card
-            style={styles.card}
-          >
+          <Card style={styles.card}>
             <Image
               source={require("../assets/images/location.png")}
               alt="Find Near Help"
@@ -66,23 +65,24 @@ export default function Index() {
             />
             <Text style={styles.quidTitle}>Find Nearby Help</Text>
             <Text style={{ textAlign: "center", marginTop: 10 }}>
-              Quickly locate hospitals, clinics, and pharmacies near your location.
+              Quickly locate hospitals, clinics, and pharmacies near your
+              location.
             </Text>
             <Link href={"/map"} asChild>
-            <Button size="lg" variant="solid" style={{ backgroundColor: "#fe2238", marginTop: 20 }}>
-              <ButtonText>Find on Map</ButtonText>
-            </Button>
+              <Button
+                size="lg"
+                variant="solid"
+                style={{ backgroundColor: "#fe2238", marginTop: 20 }}
+              >
+                <ButtonText>Find on Map</ButtonText>
+              </Button>
             </Link>
           </Card>
         </GridItem>
         <GridItem
           _extra={{ className: "col-span-10 md:col-span-6 lg:col-span-3" }}
         >
-          <Card
-            size="lg"
-            variant="filled"
-            style={styles.card}
-          >
+          <Card size="lg" variant="filled" style={styles.card}>
             <Image
               source={require("../assets/images/phone-call.png")}
               alt="Find Near Help"
@@ -117,22 +117,22 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginLeft: 20,
     marginRight: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
-    borderColor: '#fe2238',
+    borderColor: "#fe2238",
     borderWidth: 1,
-    shadowColor: '#fe2238',
+    shadowColor: "#fe2238",
     shadowOffset: { width: 2, height: 4 },
     shadowOpacity: 0.5,
     shadowRadius: 4,
     elevation: 5,
-    alignItems: 'center',
+    alignItems: "center",
   },
   quidTitle: {
-    color: '#fe2238',
+    color: "#fe2238",
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 15,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
